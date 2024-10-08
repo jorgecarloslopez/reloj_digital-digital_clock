7ff7133a71f5a7dfe6c5ac70a3558f772b8b13f6
jorgecarloslopez <lopez.cs.jorge@gmail.com>
2024-10-07 22:51:19 -0300

    I am adding the date object, then I added three methods

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function actualizarReloj() {
   const reloj = document.getElementById("reloj");
+
+  const ahora = new Date();
+  let horas = ahora.getHours();
+  let minutos = ahora.getMinutes();
+  let segundos = ahora.getSeconds();
 }
